62fb9376478be115a1d53291db78d9be1d01b23f
Hanna Golovashkina <108117285+HannaGolovashkina@users.noreply.github.com>
2023-04-16 18:16:37 +0300

    Update firebase.js

--- src/Api/firebase.js
+++ src/Api/firebase.js
@@ -6,13 +6,21 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBsyFlWp1L_zcgSy2wlSOqpqsVC8ZDUdNs",
-    authDomain: "green-grass-382016.firebaseapp.com",
-    projectId: "green-grass-382016",
-    storageBucket: "green-grass-382016.appspot.com",
-    messagingSenderId: "867297042468",
-    appId: "1:867297042468:web:22e5e9487c452384b5b8b1",
-    measurementId: "G-2PCP5Z9BFM"
+  apiKey: "AIzaSyBsyFlWp1L_zcgSy2wlSOqpqsVC8ZDUdNs",
+  authDomain: "green-grass-382016.firebaseapp.com",
+  databaseURL: "https://green-grass-382016-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "green-grass-382016",
+  storageBucket: "green-grass-382016.appspot.com",
+  messagingSenderId: "867297042468",
+  appId: "1:867297042468:web:22e5e9487c452384b5b8b1",
+  measurementId: "G-2PCP5Z9BFM"
+//     apiKey: "AIzaSyBsyFlWp1L_zcgSy2wlSOqpqsVC8ZDUdNs",
+//     authDomain: "green-grass-382016.firebaseapp.com",
+//     projectId: "green-grass-382016",
+//     storageBucket: "green-grass-382016.appspot.com",
+//     messagingSenderId: "867297042468",
+//     appId: "1:867297042468:web:22e5e9487c452384b5b8b1",
+//     measurementId: "G-2PCP5Z9BFM"
   };  
   
 // Initialize Firebas
